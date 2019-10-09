cf0364709cfecbd191c0c8624677d6b095013903
Filip Barańczuk <f.baranczuk@gmail.com>
2019-10-09 16:28:22 +0200

    Fix dice loss fn

--- networks/losses/dice.py
+++ networks/losses/dice.py
@@ -1,10 +1,7 @@
 import torch
 
-def dice_coef_loss(y_true, y_pred):
-    def dice_coef(y_true, y_pred, smooth=1.0):
-        return (2 * torch.sum(y_true * y_pred) + 1) / (torch.sum(y_true + y_pred) + 1)
+def dice_coef_loss(y_hat, y_true):
+    def dice_coef(y_hat, y_true, smooth=1.0):
+        return (2 * torch.sum(y_true * y_hat) + 1) / (torch.sum(y_true + y_hat) + 1)
 
-    return 1 - dice_coef(y_true, y_pred)
-
-def weighted_dice_coef_loss(y_true, y_pred, weights):
-    pass
+    return 1 - dice_coef(y_hat, y_true)
